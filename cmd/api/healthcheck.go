ab71cdeb6d006e1fe50bfe2bf35e2bf56648a54c
Sparkvix <24603835+sparkyvxcx@users.noreply.github.com>
2023-07-28 11:25:04 +1000

    Change healthcheck endpoint to respond with json data

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,12 +1,30 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	// js := `{"status": "available", "environment": %q, "version": %q}`
+	// js = fmt.Sprintf(js, app.config.env, version)
+
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+
+	js, err := json.Marshal(data)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and could not process your request at the moment", http.StatusInternalServerError)
+	}
+
+	// Append a newline to the JSON data to make it easier to view in terminal.
+	js = append(js, '\n')
+
+	w.Header().Set("Content-Type", "application/json")
+
+	w.Write([]byte(js))
 }
