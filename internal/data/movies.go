2b1cdac2cc048426d4a2a37490a5d613a9d2145d
Sparkvix <24603835+sparkyvxcx@users.noreply.github.com>
2023-08-30 23:53:52 +1000

    Use dynamic filtering in SQL query to implement filter lists

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -158,13 +158,17 @@ func (m MovieModel) Delete(id int64) error {
 }
 
 func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*Movie, error) {
-	query := `SELECT id, created_at, title, year, runtime, genres, version FROM movies ORDER BY id`
+	query := `SELECT id, created_at, title, year, runtime, genres, version
+	 		FROM movies
+	 		WHERE (LOWER(title) = LOWER($1) OR $1 = '')
+	 		AND (genres @> $2 OR $2 = '{}')
+			ORDER BY id`
 
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
 	// Use QueryContext() to execute the query. This returns a sql.Rows resultset containing the result.
-	rows, err := m.DB.QueryContext(ctx, query)
+	rows, err := m.DB.QueryContext(ctx, query, title, pq.Array(genres))
 	if err != nil {
 		return nil, err
 	}
@@ -198,10 +202,8 @@ func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*M
 		if err = rows.Err(); err != nil {
 			return nil, err
 		}
-
-		return movies, nil
 	}
-	return nil, nil
+	return movies, nil
 }
 
 type MockMovieModel struct{}
