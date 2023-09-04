c50c790ab4a6b176f43b897135b83f696df39d7c
Sparkvix <24603835+sparkyvxcx@users.noreply.github.com>
2023-09-04 20:55:49 +1000

    Add helper function in filter to implement sorting

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"fmt"
 	"time"
 
 	"greenlight.sparkyvxcx.co/internal/validator"
@@ -158,11 +159,12 @@ func (m MovieModel) Delete(id int64) error {
 }
 
 func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*Movie, error) {
-	query := `SELECT id, created_at, title, year, runtime, genres, version
+	query_format := `SELECT id, created_at, title, year, runtime, genres, version
 	 		FROM movies
 	 		WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '')
 	 		AND (genres @> $2 OR $2 = '{}')
-			ORDER BY id`
+			ORDER BY %s %s, id ASC`
+	query := fmt.Sprintf(query_format, filters.sortColumn(), filters.sortDirection())
 
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
