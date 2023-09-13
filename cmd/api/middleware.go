aa51a19ebd74d9907da94d7136f94f5cf9446216
Sparkvix <24603835+sparkyvxcx@users.noreply.github.com>
2023-09-13 23:21:37 +1000

    Introduce IP based rate limiter with built-in map

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
+	"sync"
 
 	"golang.org/x/time/rate"
 )
@@ -27,15 +29,41 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 }
 
 func (app *application) rateLimit(next http.Handler) http.Handler {
-	// Initialize a new rate limiter which allows an average of 2 requests per second,
-	// with maximum of 4 requests in a single 'burst'.
-	limiter := rate.NewLimiter(2, 4)
+	var (
+		mux     sync.Mutex
+		clients = make(map[string]*rate.Limiter)
+	)
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if !limiter.Allow() {
+		// Extract the client's IP address from the request.
+		ip, _, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
+			app.serverErrorResponse(w, r, err)
+			return
+		}
+
+		// Lock the mutex to prevent this code from being executed concurrently.
+		mux.Lock()
+
+		// Check to see if the IP address already exists in the map. If it doesn't, then
+		// initialize a new rate limiter and add the IP address and limiter to the map.
+		if _, found := clients[ip]; !found {
+			// Initialize a new rate limiter which allows an average of 2 requests per second,
+			// with maximum of 4 requests in a single 'burst'.
+			clients[ip] = rate.NewLimiter(2, 4)
+		}
+
+		if !clients[ip].Allow() {
+			mux.Unlock()
 			app.rateLimitExceededResponse(w, r)
 			return
 		}
+
+		// Very importantly, unlock the mutex before calling the next handler in the chain.
+		// Notice that we DON't use defer to unlock the mutex, as that would mean that the
+		// mutex isn't unlocked until all the handlers downstream of this middleware have
+		// also returned
+		mux.Unlock()
 
 		next.ServeHTTP(w, r)
 	})
