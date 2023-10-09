1cca3810fee64c9dcf70692f78bfab451d5c4456
Sparkvix <24603835+sparkyvxcx@users.noreply.github.com>
2023-10-09 23:16:12 +1100

    Add token generation endpoint based on valid credentials

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -32,6 +32,9 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
 	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
 
+	// Endpoints related to token
+	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
+
 	// Wrap the router with the panic recovery middleware
 	return app.recoverPanic(app.rateLimit(router))
 }
