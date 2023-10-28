a446e763de255e604f866cae0d13faf11036cc71
Sparkvix <24603835+sparkyvxcx@users.noreply.github.com>
2023-10-28 23:18:08 +1100

    Split part of checks from require actived user middleware

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -160,7 +160,7 @@ func (app *application) authenticate(next http.Handler) http.Handler {
 	})
 }
 
-func (app *application) requireActivatedUser(next http.HandlerFunc) http.HandlerFunc {
+func (app *application) requireAuthenticatedUser(next http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// User the contextGetUser() helper to retrieve the user information from the request context.
 		user := app.contextGetUser(r)
@@ -172,6 +172,14 @@ func (app *application) requireActivatedUser(next http.HandlerFunc) http.Handler
 			return
 		}
 
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) requireActivatedUser(next http.HandlerFunc) http.HandlerFunc {
+	fn := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		user := app.contextGetUser(r)
+
 		// If the user is not activated, use the inactiveAccountResponse() helper to inform them that
 		// they need to activate their account.
 		if !user.Activated {
@@ -182,4 +190,6 @@ func (app *application) requireActivatedUser(next http.HandlerFunc) http.Handler
 		// Call the next handler in the chain.
 		next.ServeHTTP(w, r)
 	})
+
+	return app.requireAuthenticatedUser(fn)
 }
