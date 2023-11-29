62bd3fab241cc6e28e04d1b7490fb75424a6c2c0
Sparkvix <24603835+sparkyvxcx@users.noreply.github.com>
2023-11-29 16:39:21 +1100

    Fix migration down error issue

--- create_records.sh
+++ create_records.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
+echo $1
+
 BODY='{"title":"Moana","year":2016,"runtime":"107 mins", "genres":["animation","adventure"]}'
-curl -i -d "$BODY" localhost:4000/v1/movies
+curl -d "$BODY" localhost:4000/v1/movies -H "Authorization: Bearer $1"
 
 BODY='{"title":"Black Panther","year":2018,"runtime":"134 mins","genres":["action","adventure"]}'
-curl -d "$BODY" localhost:4000/v1/movies
+curl -d "$BODY" localhost:4000/v1/movies -H "Authorization: Bearer $1"
 
 BODY='{"title":"Deadpool","year":2016, "runtime":"108 mins","genres":["action","comedy"]}'
-curl -d "$BODY" localhost:4000/v1/movies
-
+curl -d "$BODY" localhost:4000/v1/movies -H "Authorization: Bearer $1"
 
 BODY='{"title":"The Breakfast Club","year":1986, "runtime":"96 mins","genres":["drama"]}'
-curl -d "$BODY" localhost:4000/v1/movies
+curl -d "$BODY" localhost:4000/v1/movies -H "Authorization: Bearer $1"
--- migrations/000006_add_permissions.down.sql
+++ migrations/000006_add_permissions.down.sql
@@ -1,2 +1,5 @@
-DROP TABLE IF EXISTS users_permissions;
-DROP TABLE IF EXISTS permissions;
+-- ALTER TABLE users_permissions DROP CONSTRAINT IF EXISTS user_permissions_permission_id_fkey;
+-- ALTER TABLE permissions DROP CONSTRAINT IF EXISTS permissions_permission_id_fkey;
+-- ALTER TABLE users DROP CONSTRAINT IF EXISTS users_permission_id_fkey;
+DROP TABLE IF EXISTS users_permissions cascade;;
+DROP TABLE IF EXISTS permissions cascade;;
